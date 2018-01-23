c3e6ed46995d186385a0f18e3752d559795fcdad
tamplleer <tamplleer@mail.ru>
2018-01-23 23:17:13 +0600

    Initial commit

---
+++ app/src/main/java/com/tamplleer/testrussian/MainActivity.java
@@ -0,0 +1,169 @@
+package com.tamplleer.testrussian;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView t,point;
+    char strToArray[];
+    int screenWidth, screenHeight;
+    int lengthINscore=0;
+    ConstraintLayout constraintLayout;
+    char a[]={'А','И', 'Е', 'Ё', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я'};
+    String word1[] ;
+    int steps=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        word1 = S.allWord.split(",");
+        t = (TextView) findViewById(R.id.text);
+        point = (TextView) findViewById(R.id.points);
+       constraintLayout = (ConstraintLayout)findViewById(R.id.constrainlayout);
+        getScreenSize();
+
+
+
+
+    }
+
+    protected void getScreenSize() {
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+    }
+
+
+    public void exit(View view) {
+        System.exit(0);
+    }
+
+    public void start(View view) {
+        S.click = true;
+        S.someClick=false;
+        S.win=0;
+        lengthINscore=word1.length;
+        word1 = S.allWord.split(",");
+        point.setText(S.win+"/"+lengthINscore);
+        S.changeWord = 1;
+        setWordinMas();
+        constraintLayout.setBackgroundColor(ContextCompat
+                .getColor(this, R.color.Khaki));
+        t.setText("");
+    }
+
+    public void someword(View view) {
+        S.someClick=true;
+        S.click = false;
+        S.win=0;
+        int maxLength=30;
+        lengthINscore=maxLength;
+        for (int i = 1; i < maxLength; i++){
+           word1[i]=word1[(int) (Math.random()*word1.length)];
+        }
+
+        point.setText(S.win+"/"+lengthINscore);
+        S.changeWord = 1;
+        setWordinMas();
+        constraintLayout.setBackgroundColor(ContextCompat
+                .getColor(this, R.color.Khaki));
+t.setText("");
+
+    }
+
+    public void next(View view) {
+
+        if (S.click == true ||S.someClick==true) {
+            steps++;
+            S.changeWord++;
+            constraintLayout.setBackgroundColor(ContextCompat
+                    .getColor(this, R.color.Khaki));
+            if (S.right==true){
+                point.setText(S.win+"/"+lengthINscore);
+            }
+            else {constraintLayout.setBackgroundColor(ContextCompat
+                    .getColor(this, R.color.FireBrick));
+            }
+            t.setText(""+S.wordtoscreen);
+            setWordinMas();
+            if (steps==lengthINscore){ t.setText("GAME OVEr  "+S.win+"  OF  "+lengthINscore+"    Try again");
+                S.click =false;
+                S.someClick=false;
+            }
+        }
+    }
+
+    public void setWordinMas() {
+        String word[] = new String[word1.length];
+        for (int i = 1; i < word.length; i++) {
+            word[i] = word1[i];
+            if (S.changeWord == i) {
+                S.stringTOchar = word[i];
+                S.wordtoscreen=word[i];
+               createWord();
+            }
+        }
+
+    }
+
+    public void rextV(View view) {
+        t.setText("!!!!!!!!!!!!");
+    }
+
+    public void createWord() {
+        strToArray = S.stringTOchar.toCharArray();
+        char strArray00[]=new char[strToArray.length];
+
+        for (int i = 0; i < strToArray.length; i++) {
+            if (strToArray[i]==a[0] ||
+            strToArray[i]==a[1]||
+            strToArray[i]==a[2]||
+            strToArray[i]==a[3]||
+            strToArray[i]==a[4]||
+            strToArray[i]==a[5]||
+            strToArray[i]==a[6]||
+            strToArray[i]==a[7]||
+            strToArray[i]==a[8]||
+            strToArray[i]==a[9])S.delet=i;
+            strArray00[i]=' ';
+            if (Character.isLowerCase(strToArray[i]))
+                strToArray[i] = Character.toUpperCase(strToArray[i]);
+        }
+        Text text[] = new Text[ strToArray.length+1];
+       // strToArray[999]=' ';
+        //text[strToArray.length+1]= new Text(this,30,30,strToArray[999],30,999);
+
+        int x1 = screenWidth / strToArray.length ;
+        int y = screenHeight / 2;
+        int x=0;
+        for (int i = 0; i < strToArray.length; i++) {
+
+            x = x1 *i+30;
+            if (i==0)x=30;
+
+            text[i] = new Text(this, x, y, strToArray[i],30,i);
+        }
+
+        for (int i = 0; i < strToArray.length; i++) {
+           strToArray[i]=strArray00[i];
+
+        }
+    }
+}
---
+++ app/src/main/java/com/tamplleer/testrussian/Text.java
@@ -0,0 +1,68 @@
+package com.tamplleer.testrussian;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by tampl on 22.01.2018.
+ */
+
+public class Text extends MainActivity {
+    TextView t;
+    char ru;
+    Context context;
+    int x, y, size, type;
+    char count;
+    ConstraintLayout constraintLayout;
+    float alpha = 1; // размер картинки, врещение, прозрачность
+
+    Text(MainActivity main, int x, int y, char count, int size, int type) {
+        this.x = x;
+        context = main;
+        this.y = y;
+        this.size = size;
+        this.type = type;
+        this.count = count;
+        S.charTextV = new TextView(main);
+        S.charTextV.setAlpha(alpha);
+        S.charTextV.setX(x); // координаты
+        S.charTextV.setY(y);
+        S.charTextV.setBackgroundResource(R.color.DarkSlateGray);
+        S.charTextV.setTextColor(Color.rgb(255, 228, 181));
+        S.charTextV.setScaleX(1.5f); // изменение размеров в 1.5 раза
+        S.charTextV.setScaleY(1.5f);
+        S.charTextV.setText("" + count);
+        ru = count;
+        S.type = type;
+        t = (TextView) main.findViewById(R.id.text);
+        constraintLayout = (ConstraintLayout) main.findViewById(R.id.constrainlayout);
+        //      constraintLayout = (ConstraintLayout) findViewById(R.id.constrainlayout);
+        main.addContentView(S.charTextV, new RelativeLayout.LayoutParams(size * 4, size));
+        S.charTextV.setOnClickListener(new View.OnClickListener() {  // вешаем слушателя на клик
+            public void onClick(View v) {
+                touched(); // метод-обработчик нажатия
+            }
+        });
+    }
+
+    void touched() {
+        //     MainActivity main1 = new MainActivity();
+        //    main1.getWindow().setBackgroundDrawableResource(R.drawable.fotodena);
+        t.setText("" + count + " ?");
+        if (type == S.delet) {
+            S.win++;
+            S.right = true;
+            //constraintLayout.setBackgroundColor(ContextCompat
+            //    .getColor(context, R.color.CHETO));
+        } else S.right = false;
+        //  FrameLayout parent = (FrameLayout) S.charTextV.getParent();
+        // parent.removeView(S.charTextV);
+    }
+}
